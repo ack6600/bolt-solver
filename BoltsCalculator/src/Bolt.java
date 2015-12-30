058fa738639c90b1260096c9d361875bc271cbb4
Adam Urish <adamurish@gmail.com>
2015-12-29 20:15:02 -0600

    Project

---
+++ BoltsCalculator/src/Bolt.java
@@ -0,0 +1,77 @@
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+@SuppressWarnings("unused")
+public class Bolt {
+	int[] sides;
+	int sOne;
+	int sTwo;
+	int sThree;
+	int sFour;
+	int sFive;
+	int sSix;
+	int type;
+	int test;
+	public Bolt(int sideNumber)
+	{
+		Dimension d = new Dimension();
+		d.height = 3;
+		d.width = 3;
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		for (int i = 1;i<sideNumber+1;i++)
+		{
+			l.add(i);
+		}
+		type = 0;
+	}
+	public Bolt()
+	{
+		//init just for method use
+		type = 2;
+	}
+	public Bolt(int sideOne,int sideTwo,int sideThree,int sideFour,int sideFive,int sideSix)
+	{
+		sOne = sideOne;
+		sTwo = sideTwo;
+		sThree = sideThree;
+		sFour = sideFour;
+		sFive = sideFive;
+		sSix = sideSix;
+		type = 1;
+	}
+	public String getTypeS()
+	//returns type in string
+	{
+		if(type==0)
+		{
+			return "custom";
+		}
+		else if (type == 1)
+		{
+			return "standard";
+		}
+		else
+		{
+			return "dummy";
+		}
+	}
+	public int getTypeI()
+	//returns type as integer
+	//0 is custom 1 is standard
+	{
+		return type;
+	}
+	public void print(Bolt b)
+	//prints bolt to console if type 1
+	{
+		if (b.type == 1)
+		{
+		System.out.println("  "+b.sOne);
+		System.out.println(b.sTwo+"   "+b.sSix);
+		System.out.println(b.sThree+"   "+b.sFive);
+		System.out.println("  "+b.sFour);
+		}
+		
+	}
+
+}
---
+++ BoltsCalculator/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Bolt writerBolt = new Bolt();
+		Bolt bolt1 = new Bolt(1,3,5,4,2,6);
+		Bolt bolt2 = new Bolt(2,3,5,6,1,4);
+		Bolt bolt3 = new Bolt(2,3,5,6,1,4);
+		Bolt bolt4 = new Bolt(2,3,5,6,1,4);
+		Bolt bolt5 = new Bolt(2,3,5,6,1,4);
+		Bolt bolt6 = new Bolt(2,3,5,6,1,4);
+		writerBolt.print(bolt1);
+		
+	}
+
+}
